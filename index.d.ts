842d8f20e589cba722116df5a1ae5f3773af1c06
Brian Seymour <imbrianseymour@gmail.com>
2024-09-11 13:40:48 -0500

    fix types, fix exports, add json library module, fix env expand, version bump

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,31 @@ declare module '@exajs/core' {
     const exa: any;
     export default exa;
 }
+
+declare module '@exajs/core/database' {
+    const connection: any;
+    const models: any;
+    export { connection, models };
+}
+
+declare module '@exajs/core/database/models' {
+    const models: any;
+    export default models;
+}
+
+declare module '@exajs/core/database/connection' {
+    const connection: any;
+    export default connection;
+}
+
+declare module '@exajs/core/system/sequelize' {
+    const sequelize: any;
+    export default sequelize;
+    export * from 'sequelize';
+}
+
+declare module '@exajs/core/util' {
+    import json from '#exa/util/json.js';
+
+    export { json };
+}
