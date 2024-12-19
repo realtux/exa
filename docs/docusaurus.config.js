6c9b3b4197ea0e9114b992813a3bfe40cc8eaf33
Brian Seymour <imbrianseymour@gmail.com>
2024-12-19 14:20:05 -0600

    update github action

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: 'https://exa.js.org',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
