385b41a797ea0f6cc481c79201c3de8b03052ee2
Brian Seymour <imbrianseymour@gmail.com>
2025-04-23 15:00:08 -0500

    convert hooks to async, added new console option called exit to determine if process.exit() should be called automatically at the end of the command

--- var/template/config/hooks.js
+++ var/template/config/hooks.js
@@ -1,12 +1,17 @@
 export default new class {
 
     // runs before starting any modules but after configuration was loaded
-    before_startup() {
+    async before_startup() {
 
     }
 
     // runs after all modules have finished starting up
-    after_startup() {
+    async after_startup() {
+
+    }
+
+    // runs before a console command terminates
+    async before_shutdown() {
 
     }
 
